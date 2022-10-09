c4cb23f413d041387f410f17d9fa4adc42cffce5
cjwarrior7 <51403051+cjwarrior7@users.noreply.github.com>
2022-10-09 16:24:04 +0530

    Again update sum

--- sum.js
+++ sum.js
@@ -2,7 +2,7 @@ const num2 = 5;
 const num3 = 3;
 
 // add two numbers
-const sum = num2 + num3;
+const sum1 = num2 + num3;
 
 // display the sum
-console.log('The sum of ' + num2 + ' and ' + num3 + ' is: ' + sum);
+console.log('The sum of ' + num2 + ' and ' + num3 + ' is: ' + sum1);
